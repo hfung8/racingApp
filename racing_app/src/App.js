9e5f3391c5beab7cdcb21def1ff38dbc30c5c217
Harrison Fung <harrison.hok.fung@gmail.com>
2018-09-25 21:44:54 -0400

    initial commit

---
+++ racing_app/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Weather from './components/Weather';
+import WeatherForm from './components/WeatherForm';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Weather />;
+        <WeatherForm />;
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ racing_app/src/components/Weather.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+// var WeatherForm = require('./WeatherForm.js');
+import WeatherMessage from './WeatherMessage';
+
+class Weather extends Component{
+    render (){
+    return (
+        <div>
+        <WeatherMessage />;     
+        </div>
+    )
+    }
+}
+
+export default Weather;
